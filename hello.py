85df8f086c8dbf72a7ba52ab2f7b968eb4dba84e
fatiharidlo <fatiharidlo16>
2026-09-07 11:41:17 +0700

    nambah hello

--- hello.py
+++ hello.py
@@ -2,4 +2,5 @@ import sys
 print("Praktek Pemrograman Terapan - pertemuan 1")
 print("NAMA :", "Muhammad Fatih Ar'Ridlo")
 print("NIM :", "33132501001")
+print("hello world")
 print("Python", " sys.version")
